ed2425c4b6c1d761ff3c45d4eb80b0067ec6235e
Akshum <akshumgarg20@gmail.com>
2026-02-15 15:25:01 +0530

    Update CORS

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://YOUR_USERNAME.github.io'
+  ],
+  credentials: true
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
